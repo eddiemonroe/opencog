0b2a3e7ccb74c9218d9de96aa8b30ae160ec61f6
Eddie Monroe <eddiemonroe@gmail.com>
2015-09-28 14:25:38 -0400

    correct type in function name, add rule for IntensionalEquivalence

--- opencog/reasoning/pln/rules/equivalence-transformation-rule.scm
+++ opencog/reasoning/pln/rules/equivalence-transformation-rule.scm
@@ -13,6 +13,13 @@
 ;       B
 ;       A
 ;
+; Due to pattern matching issues, currently the file has been divided into 3
+; parts, each pertaining to different links. The rules are :-
+;       pln-rule-equivalence-transformation
+;       pln-rule-intensional-equivalence-transformation
+;       pln-rule-extensional-equivalence-transformation
+;
+;
 ; -----------------------------------------------------------------------------
 
 (define pln-rule-equivalence-transformation
@@ -28,18 +35,21 @@
             (ListLink
                 (AndLink
                     (ImplicationLink
-                        (ConceptNode "$A")
-                        (ConceptNode "$B"))
+                        (VariableNode "$A")
+                        (VariableNode "$B"))
                     (ImplicationLink
-                        (ConceptNode "$B")
-                        (ConceptNode "$A")))
+                        (VariableNode "$B")
+                        (VariableNode "$A")))
                 (EquivalenceLink
                     (VariableNode "$A")
                     (VariableNode "$B"))))))
 
-(define (pln-formula-equivalence-transofrmation AII EV)
+(define (pln-formula-equivalence-transformation AII EV)
     (cog-set-tv!
-        AII (cog-tv EV)))
+        (list-ref (cog-outgoing-set AII) 0) (cog-tv EV))
+    (cog-set-tv!
+        (list-ref (cog-outgoing-set AII) 1) (cog-tv EV))
+    AII)
 
 ; Name the rule
 (define pln-rule-equivalence-transformation-name
@@ -47,3 +57,36 @@
 (DefineLink
   pln-rule-equivalence-transformation-name
   pln-rule-equivalence-transformation)
+
+
+
+; IntensionalEquivalanceTransformationRule
+;
+(define pln-rule-intensional-equivalence-transformation
+    (BindLink
+        (VariableList
+            (VariableNode "$A")
+            (VariableNode "$B"))
+        (IntensionalEquivalenceLink
+            (VariableNode "$A")
+            (VariableNode "$B"))
+        (ExecutionOutputLink
+            (GroundedSchemaNode "scm:pln-formula-equivalence-transformation")
+            (ListLink
+                (AndLink
+                    (IntensionalImplicationLink
+                        (VariableNode "$A")
+                        (VariableNode "$B"))
+                    (IntensionalImplicationLink
+                        (VariableNode "$B")
+                        (VariableNode "$A")))
+                (IntensionalEquivalenceLink
+                    (VariableNode "$A")
+                    (VariableNode "$B"))))))
+
+; Name the rule
+(define pln-rule-intensional-equivalence-transformation-name
+  (Node "pln-rule-intensional-equivalence-transformation"))
+(DefineLink
+  pln-rule-intensional-equivalence-transformation-name
+  pln-rule-intensional-equivalence-transformation)
